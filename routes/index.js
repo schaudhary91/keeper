337fc8d673d878a91e2d01524547def38aa9b202
Sandeep Chaudhary <schaudhary_91@hotmail.com>
2014-12-09 13:00:58 +0530

    changed order in router

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
 /*GET Hello World*/
 router.get('/hw', function(req, res) {
   res.render('hw', { title: 'Hello World' });
 });
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
 
 module.exports = router;
