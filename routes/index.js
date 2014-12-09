bd096d959b054881a600222dc5c9d26f61050edd
Sandeep Chaudhary <schaudhary_91@hotmail.com>
2014-12-09 12:56:42 +0530

    Added test page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+/*GET Hello World*/
+router.get('/hw', function(req, res) {
+  res.render('hw', { title: 'Hello World' });
+});
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
