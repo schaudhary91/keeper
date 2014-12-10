134f4b18aa98d089f506b7802e54408ae03fff96
Sandeep Chaudhary <schaudhary_91@hotmail.com>
2014-12-10 14:41:08 +0530

    Added route to get data

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 keeper
 ======
 1. "npm install" to install all the dependencies.
-2. "npm start" to start the server
+2. "npm start" to start the server and "DEBUG=express ./bin/www" to start app in debug mode
 3. Access website at http://localhost:3000
--- routes/data.js
+++ routes/data.js
@@ -7,16 +7,53 @@ module.exports = function (app){
 
   var db = mongoskin.db('mongodb://@localhost:27017/keeper', {safe:true})
 
+  function getCollection(cname){
+    return db.collection(cname);
+  }
+
+  function getData(cname,query,projection){
+    getCollection(cname).find(query,projection);
+  }
+
+  app.param('cn', function(req, res, next, cn){
+    req.collection = getCollection(cn);
+    return next();
+  })
+
   /* GET success true */
   router.get('/', function(req, res) {
     res.send({success:true});
   });
 
-  /* GET success true */
-  router.get('/test', function(req, res) {
-    res.send({success:true});
+  /* GET Collection Data */
+  router.get('/:cn', function(req, res, next) {
+    getCollection(req.params.cn).find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+      if (e) {
+        return next(e);
+      }
+      res.send(results);
+    })
   });
 
+  /* GET Collection Data by uid*/
+  router.get('/:cn/:uid', function(req, res, next) {
+    getCollection(req.params.cn).find({uid:req.params.uid} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+      if (e) {
+        return next(e);
+      }
+      res.send(results);
+    })
+  });
+
+  // app.get('/cn/:cn', function(req, res, next) {
+  //   req.collection.find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+  //     if (e) {
+  //       return next(e);
+  //     }
+  //     res.send(results);
+  //   })
+  // })
+
   return router;
-  
+
 };
