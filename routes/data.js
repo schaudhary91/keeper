6ab34ae3adb5c117e4c02fc143e9073332bac52d
Sandeep Chaudhary <schaudhary_91@hotmail.com>
2014-12-10 17:47:56 +0530

    Added more data

--- routes/data.js
+++ routes/data.js
@@ -28,21 +28,73 @@ module.exports = function (app){
   /* Generate test data */
   router.get('/gentestd', function(req, res) {
     var users = [
-      {"firstname":"shashaank","lastname":"kumar","uid":"skum"},
-      {"firstname":"sandeep","lastname":"chaudhary","uid":"scha"}
+      {
+        "email":"skumar@gmail.com",
+        "firstname":"shashaank",
+        "lastname":"kumar",
+        "uid":"s1234x",
+        "uname":"skum"
+      },
+      {
+        "email":"scha@gmail.com",
+        "firstname":"sandeep",
+        "lastname":"chaudhary",
+        "uid":"s3456x",
+        "uname":"scha"
+      }
     ];
-
+    var tags = [
+      {
+        "title":"This is Title",
+        "text":"some selected text.",
+        "tags":["test","dummy","xyz"],
+        "uid":"s3456x",
+        "fullurl":"http://www.google.com"
+      },
+      {
+        "title":"This is Title x",
+        "text":"some selected text. some selected text. some selected text. some selected text. some selected text.",
+        "tags":["test","long","xyz"],
+        "uid":"s1234x",
+        "fullurl":"http://www.google.com"
+      },
+      {
+        "title":"This is Title y",
+        "text":"some selected text.",
+        "tags":["test","sha"],
+        "uid":"s1234x",
+        "fullurl":"http://www.google.com"
+      },
+      {
+        "title":"This is Title z",
+        "text":"some selected text. some selected text. some selected text. some selected text. some selected text.",
+        "tags":["test","sand","xyz"],
+        "uid":"s3456x",
+        "fullurl":"http://www.google.com"
+      }
+    ];
 
     getCollection('tusers').remove({},function(er){
       if(!er){
         console.log('tusers deleted');
       }
     });
-
+    getCollection('ttags').remove({},function(er){
+      if(!er){
+        console.log('ttags deleted');
+      }
+    });
     for(var i = 0; i<users.length; i++){
       getCollection('tusers').insert(users[i],function(er){
         if(!er){
           console.log('tusers created');
+        }
+      });
+    }
+    for(var j = 0; j<tags.length; j++){
+      getCollection('ttags').insert(tags[j],function(er){
+        if(!er){
+          console.log('ttags created');
         }
       });
     }
