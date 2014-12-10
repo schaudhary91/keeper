e14b59ae92f3d37d28e0cd8fc5b7641ae4f1712f
Sandeep Chaudhary <schaudhary_91@hotmail.com>
2014-12-10 15:13:42 +0530

    Test Data generator

--- routes/data.js
+++ routes/data.js
@@ -25,6 +25,27 @@ module.exports = function (app){
     res.send({success:true});
   });
 
+  /* Generate test data */
+  router.get('/gentestd', function(req, res) {
+    var users = [
+      {"firstname":"shashaank","lastname":"kumar","uid":"skum"},
+      {"firstname":"sandeep","lastname":"chaudhary","uid":"scha"}
+    ];
+
+    if(db.tusers){
+      db.tusers.remove({});
+    }
+
+    for(var i = 0; i<users.length; i++){
+      getCollection('tusers').insert(users[i],function(er){
+        if(!er){
+          console.log('user created');
+        }
+      });
+    }
+    res.send('Test data created');
+  });
+
   /* GET Collection Data */
   router.get('/:cn', function(req, res, next) {
     getCollection(req.params.cn).find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
@@ -36,7 +57,7 @@ module.exports = function (app){
   });
 
   /* GET Collection Data by uid*/
-  router.get('/:cn/:uid', function(req, res, next) {
+  router.get('/:cn/uid/:uid', function(req, res, next) {
     getCollection(req.params.cn).find({uid:req.params.uid} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
       if (e) {
         return next(e);
@@ -44,15 +65,6 @@ module.exports = function (app){
       res.send(results);
     })
   });
-
-  // app.get('/cn/:cn', function(req, res, next) {
-  //   req.collection.find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
-  //     if (e) {
-  //       return next(e);
-  //     }
-  //     res.send(results);
-  //   })
-  // })
 
   return router;
 
